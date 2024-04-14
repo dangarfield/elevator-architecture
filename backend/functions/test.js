71708b23ba0c8b14d7f3a0ed55a7a54893cc9dc3
Dan Garfield <dangarfielduk@hotmail.com>
2024-04-14 13:26:14 +0100

    async function handler

--- backend/functions/test.js
+++ backend/functions/test.js
@@ -1,22 +1,31 @@
 
 // const { MongoClient } = require("mongodb");
 import { MongoClient } from "mongodb";
-const mongoClient = new MongoClient(process.env.MONGODB_URI)
+// const mongoClient = new MongoClient(process.env.MONGODB_URI)
+const mongoClient = new MongoClient('mongodb+srv://arch:arch@cluster0.bjaxcux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+
+
 
 const clientPromise = mongoClient.connect();
 
-const handler = async (event) => {
+export async function handler (event, context) {
     try {
         const database = (await clientPromise).db('skills');
         const collection = database.collection('skills');
         const one = await collection.findOne()
-        return Response.json({one})
+        // return Response.json({one}, {statusCode:200})
+        return { statusCode: 500, body: one.data.toString() }
     } catch (error) {
         console.log('error', error)
-        return new Response('Error')
-        // return { statusCode: 500, body: error.toString() }
+        // return new Response('Error')
+        return { statusCode: 500, body: error.toString() }
     }
 }
 
-export default handler
+// export default handler
 // module.exports = { handler }
+
+// export async function handler (event, context) {
+//   return await app.run(event, context)
+// }
